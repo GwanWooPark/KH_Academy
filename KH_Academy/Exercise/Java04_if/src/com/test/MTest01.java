da952e7a05eef66648b0a5da909abbeb1fb2846a
GwanWooPark <pgw4712@icloud.com>
2021-01-18 15:56:01 +0900

    commit

---
+++ KH_Academy/Exercise/Java04_if/src/com/test/MTest01.java
@@ -0,0 +1,18 @@
+package com.test;
+
+public class MTest01 {
+    public static void main(String[] args) {
+        //if : 만약 ~라면
+
+        prn01();
+    }
+
+    public static void prn01() {
+        int i = 10;
+        int j = 20;
+
+        if(i >= 10) {
+            System.out.println("i는 10보다 크거나 같습니다.");
+        }
+    }
+}

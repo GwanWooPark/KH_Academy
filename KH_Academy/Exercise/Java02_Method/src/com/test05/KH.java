109aab7e9807c89df4d14b64b53f66ea4b08c2f4
GwanWooPark <pgw4712@icloud.com>
2021-01-20 22:35:21 +0900

    Java02_Method

--- KH_Academy/Exercise/Java02_Method/src/com/test05/KH.java
+++ KH_Academy/Exercise/Java02_Method/src/com/test05/KH.java
@@ -6,10 +6,18 @@ public class KH {
     // 3. QClass에다가 전화번호 가장 뒷 4자리를 받아서, "xxxx번호님 안녕하세요."를 리턴하는 getPhone이라는 메소드를  만들자.
     // 상속인 경우 상속된 곳에서 / 아닌경우 같은 패키지 내에서 접근 가능하고, non-static 영역에 저장되는 메소드
 
-    String name = "박관우";
-    int phone = 4712;
 
     // 4. name 변수와 phone 변수를 사용하여, QClass에 만들어 놓은 getName과 getPhone을 호출하고 리턴받은 값들을 출력하자.
+    public static void main(String[] args) {
 
+
+        String name = "박관우";
+        int phone = 4712;
+
+        QClass qClass = new QClass();
+
+        System.out.println(QClass.getName(name));
+        System.out.println(qClass.getPhone(Integer.toString(phone)));
+    }
 
 }
---
+++ KH_Academy/Exercise/Java02_Method/src/com/test05/QClass.java
@@ -0,0 +1,12 @@
+package com.test05;
+
+public class QClass {
+
+    public static String getName(String name) {
+        return name;
+    }
+
+    protected String getPhone(String phone) {
+        return phone;
+    }
+}

4502ec967b950433a8ef3747012cfe49442eafe4
GwanWooPark <pgw4712@icloud.com>
2021-01-25 16:19:47 +0900

    Java10_Array

--- KH_Academy/Exercise/Java10_Array/src/com/test02/DeepCopy.java
+++ KH_Academy/Exercise/Java10_Array/src/com/test02/DeepCopy.java
@@ -16,7 +16,6 @@ public class DeepCopy {
         for (int i = 0; i < original.length; i++)
             copyOne[i] = original[i];
 
-
         System.out.println(original[1]);
         System.out.println(copyOne[1]);
         System.out.println(Arrays.toString(original));

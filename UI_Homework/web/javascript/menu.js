5f386c33a82da4ae4e10ca780aec51fb5ba86b8d
GwanWooPark <pgw4712@icloud.com>
2021-03-09 01:59:08 +0900

    HTML

---
+++ UI_Homework/web/javascript/menu.js
@@ -0,0 +1,20 @@
+function show_plus(id) {
+    var obj = document.getElementById(id);
+
+    if (obj.style.display === "none") {
+        obj.style.display = "block";
+    } else {
+        obj.style.display = "none";
+    }
+}
+
+function show_my(id) {
+    var obj = document.getElementById(id);
+
+    if (obj.style.display === "none") {
+        obj.style.display = "block";
+    } else {
+        obj.style.display = "none";
+    }
+}
+
